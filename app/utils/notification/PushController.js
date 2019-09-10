a32fc314a589203100f74bd67af84c21639eb56d
Nhat Quang Ta <nhatquangta2911@gmail.com>
2019-09-10 14:35:53 +0700

    Demo Notification

--- app/utils/notification/PushController.js
+++ app/utils/notification/PushController.js
@@ -13,13 +13,41 @@ export default class PushController extends Component {
       // (required) Called when a remote or local notification is opened or received
       onNotification(notification) {
         console.log('NOTIFICATION:', notification);
-
         // process the notification here
       },
       // Android only
       senderID: '116963018082',
       popInitialNotification: true,
       requestPermissions: true
+    });
+
+    const localNotif = PushNotification.localNotification({
+      /* Android Only Properties */
+      id: '0',
+      ticker: 'My Notification Ticker', // (optional)
+      autoCancel: true, // (optional) default: true
+      largeIcon: 'ic_launcher', // (optional) default: "ic_launcher"
+      smallIcon: 'ic_notification',
+      bigText: 'My big text that will be shown when notification is expanded',
+      subText: 'This is a subText', // (optional) default: none
+      color: 'red', // (optional) default: system default
+      vibrate: true, // (optional) default: true
+      vibration: 300, // vibration length in milliseconds, ignored if vibrate=false, default: 1000
+      tag: 'some_tag', // (optional) add tag to message
+      group: 'group', // (optional) add group to message
+      ongoing: false, // (optional) set whether this is an "ongoing" notification
+      priority: 'high', // (optional) set notification priority, default: high
+      visibility: 'private', // (optional) set notification visibility, default: private
+      importance: 'high' // (optional) set notification importance, default: high
+    });
+
+    PushNotification.presentLocalNotification(localNotif);
+
+    PushNotification.localNotificationSchedule({
+      message: 'Recuring',
+      // eslint-disable-next-line no-mixed-operators
+      date: new Date(Date.now() + 10 * 1000),
+      color: 'red'
     });
   }
 
