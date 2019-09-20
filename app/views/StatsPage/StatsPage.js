14965033dcffbb79f5714140d996b0b1be1cc1e4
Nhat Quang Ta <nhatquangta2911@gmail.com>
2019-09-20 14:24:19 +0700

    Demo Alert

--- app/views/StatsPage/StatsPage.js
+++ app/views/StatsPage/StatsPage.js
@@ -1,15 +1,47 @@
+/* eslint-disable no-undef */
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Text } from '../../components';
+import { View, Alert } from 'react-native';
+import { Text, Button } from '../../components';
 import styles from './styles';
 
 export class StatsPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+  showAlert = () => {
+    Alert.alert(
+      'Do you want to delete this item?',
+      'It seems that there is something wrong?',
+      [
+        // { text: 'Ask me later', onPress: () => console.log('Ask me later pressed') },
+        {
+          text: 'Cancel',
+          style: 'cancel'
+        },
+        { text: 'OK', onPress: () => this.setState({ counter: ++this.state.counter }) }
+      ],
+      { cancelable: false }
+    );
+  };
+
   render() {
     const { statsContainer } = styles;
     return (
       <View style={statsContainer}>
-        <Text.CommonText>Statistic Screen</Text.CommonText>
+        <Text.TitleText>Statistic Screen</Text.TitleText>
         <Text.CommonText>Coming soon...</Text.CommonText>
+        <Text.TitleText>{this.state.counter | 0}</Text.TitleText>
+        <Button
+          title="Test Alert"
+          iconName="list"
+          onPress={() => {
+            this.showAlert();
+          }}
+          backgroundColor="#444"
+        />
       </View>
     );
   }
